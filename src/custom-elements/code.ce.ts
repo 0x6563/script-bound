607eddcf266b4420a0827ca564895131c947c333
0x6563 <63897158+0x6563@users.noreply.github.com>
2024-12-14 23:10:17 -0500

    empty

--- src/custom-elements/code.ce.ts
+++
@@ -1,6 +0,0 @@
-import { defineCustomElement } from 'vue'
-import CodeComponent from '../components/monaco.ce.vue';
-
-const Code = defineCustomElement(CodeComponent);
-
-customElements.define('code', Code);
