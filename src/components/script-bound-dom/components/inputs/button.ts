d7490e379c4d97cd1e8b528e921ab662b014001f
0x6563 <63897158+0x6563@users.noreply.github.com>
2025-01-27 23:21:55 -0500

    update button

--- src/components/script-bound-dom/components/inputs/button.ts
+++ src/components/script-bound-dom/components/inputs/button.ts
@@ -1,4 +1,5 @@
-import type { DOMNodeLike, ElementNodeLike } from "../../services/elements.ts";
+import type { ComponentController } from "../../services/controllers/component.ts";
+import type { ElementNodeLike } from "../../services/elements.ts";
 import { BaseComponent } from "../base.ts";
 
 export class Button extends BaseComponent {
@@ -7,14 +8,16 @@ export class Button extends BaseComponent {
     }
     private element?: ElementNodeLike;
 
-    connect(subcomponents: []): DOMNodeLike[] {
-        const container = this.controller.application.createNode('label');
+    connect(subcomponents: ComponentController[]) {
         this.element = this.controller
             .application
             .createNode('button', {}, { click: (e) => { this.controller.eventHandler({ event: 'action', value: null }) } });
-        container.appendChild(this.element);
-        const text = this.controller.application.createNode('div', { 'data-bound-label': '' });
-        container.appendChild(text);
-        return [container];
+        for (const component of subcomponents) {
+            const doms = component.connect();
+            for (const dom of doms) {
+                this.element.appendChild(dom);
+            }
+        }
+        return [this.element];
     }
 }
